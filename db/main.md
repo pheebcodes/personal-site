d010deb28e50d9c2fcf3e1a6ceaaca2ed26cb06a
Phoebe Clarke <me@phoebe.codes>
2022-12-06 23:01:12 -0500

    More helper functions

--- db/main.md
+++ db/main.md
@@ -8,7 +8,7 @@ before that i was architecting a new website in react for an automotive parts
 retailer chain. a long time ago i was also building websites and calendaring
 systems for public libraries across north america.
 
-outside of work i stream on twitch 3 days a week. my current schedule is tuesday 7&en;10PM,
-thursday 7&en;10PM, and saturday 2&en;5PM (all times EST). most of the time i play
+outside of work i stream on twitch 3 days a week. my current schedule is tuesday 7-10PM,
+thursday 7-10PM, and saturday 2-5PM (all times EST). most of the time i play
 games, but sometimes i code and fiddle with hobby electronics. you can find my
 up-to-date schedule on my twitch profile.
--- scripts/build.js
+++ scripts/build.js
@@ -16,45 +16,50 @@ try {
 	}
 }
 
+// Helper functions.
 const read = async (p) => await fs.readFile(p, "utf8");
 const write = async (p, d) => await fs.writeFile(path.join(BUILD_DIR, p), d);
 const copy = async (i, o = i) => await write(o, await read(i));
+const renderMd = (d) => marked(d, { smartypants: true });
+const renderHbs = (t, d) => Handlebars.compile(t)(d);
+const minifyHtml = (d) =>
+	minify(d, {
+		collapseWhitespace: true,
+		minifyCSS: true,
+		removeComments: true,
+		removeEmptyAttributes: true,
+		removeRedundantAttributes: true,
+	});
+const readMd = async (p) => {
+	const d = fm(await read(p));
+	const b = renderMd(d.body);
+	return {
+		...d.attributes,
+		body: b,
+	};
+};
+const readJson = async (p) => JSON.parse(await read(p));
 
 // Copy _redirects, license, and font files.
 await copy("LICENSE.txt");
 await copy("_redirects");
 await copy("fonts/FiraMono-Regular.ttf", "font.ttf");
 
-// Build stylesheet.
+// Read stylesheet.
 const stylesheet = await read("style.css");
 
-// Build data object to pass to template.
-const main = fm(await read("db/main.md"));
-const links = JSON.parse(await read("db/links.json"));
-const renderedBody = marked(main.body);
+// Read content.
+const content = await readMd("db/main.md");
+const links = await readJson("db/links.json");
 
 const data = {
-	main: {
-		...main.attributes,
-		toString() {
-			return renderedBody;
-		},
-	},
+	content,
 	links,
 	stylesheet,
 };
 
 // Compile + minify template.
-const templateSrc = await read("index.hbs");
-const template = Handlebars.compile(templateSrc);
-const output = template(data);
-const minifiedOutput = minify(output, {
-	collapseWhitespace: true,
-	minifyCSS: true,
-	removeComments: true,
-	removeEmptyAttributes: true,
-	removeRedundantAttributes: true,
-});
+const html = minifyHtml(renderHbs(await read("index.hbs"), data));
 
 // Write output.
-await write("index.html", minifiedOutput);
+await write("index.html", html);
