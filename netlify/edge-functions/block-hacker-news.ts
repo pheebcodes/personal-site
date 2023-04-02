5a2d0f042d8db336be1b4c56ec193326c22610fc
Phoebe Clarke <me@phoebe.codes>
2023-04-02 11:04:17 -0400

    Block news.ycombinator.com (prep for blog)

---
+++ netlify/edge-functions/block-hacker-news.ts
@@ -0,0 +1,16 @@
+function shouldBlock(url: string): boolean {
+	try {
+		return new URL(url).hostname.endsWith("ycombinator.com");
+	} catch (e) {
+		return url.includes("ycombinator.com");
+	}
+}
+
+export default function handler(request: Request): Response | undefined {
+	const refererString = request.headers.get("referer");
+	if (refererString && shouldBlock(refererString)) {
+		return new Response("hacker news sucks", {
+			status: 403,
+		});
+	}
+}
