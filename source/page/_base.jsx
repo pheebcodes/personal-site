0fefbe2845ef052cd877769054f6d950f8f7891f
Phoebe Clarke <me@phoebe.codes>
2023-05-27 12:48:31 -0400

    Update links on branch

--- source/page/_base.jsx
+++ source/page/_base.jsx
@@ -23,8 +23,8 @@ function FooterLinks() {
 				<FooterLink to="mailto:me@phoebe.codes" label="email phoebe" me>
 					email
 				</FooterLink>{" "}
-				<FooterLink to="/public.pgp" label="phoebe's pgp public key" newTab me>
-					(pgp)
+				<FooterLink to="/public.pgp" label="phoebe's public key" newTab me>
+					(key)
 				</FooterLink>
 			</span>
 			<FooterLink to="https://www.github.com/pheebcodes" label="phoebe's github profile" newTab me>
@@ -33,25 +33,16 @@ function FooterLinks() {
 			<FooterLink to="https://www.linkedin.com/in/phoebe-c-9a2b00234" label="phoebe's linkedin profile" newTab me>
 				linkedin
 			</FooterLink>
-			<FooterLink to="https://www.cohost.org/phoebecodes" label="phoebe's cohost profile" newTab me>
-				cohost
-			</FooterLink>
-			<FooterLink to="https://www.twitter.com/pheebcodes" label="phoebe's twitter profile" newTab me>
-				twitter
-			</FooterLink>
 			<FooterLink
 				to="https://www.hachyderm.io/@phoebecodes"
-				label="phoebe's mastodon profile on hachyderm.io"
+				label="phoebe's fediverse profile on hachyderm.io"
 				newTab
 				me
 			>
-				mastodon
+				fediverse
 			</FooterLink>
-			<FooterLink to="https://www.instagram.com/pheebcodes" label="phoebe's instagram profile" newTab me>
-				instagram
-			</FooterLink>
-			<FooterLink to="https://www.twitch.tv/phoebecodes" label="phoebe's twitch profile" newTab me>
-				twitch
+			<FooterLink to="https://bsky.app/profile/phoebe.codes" label="phoebe's bluesky profile" newTab me>
+				bluesky
 			</FooterLink>
 		</>
 	);
