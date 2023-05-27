3ef91e5d8038bdcddfb4b05f58ad02f44364047f
Phoebe Clarke <me@phoebe.codes>
2023-05-27 15:20:15 -0400

    Space out utils a bit

--- source/util.js
+++ source/util.js
@@ -6,28 +6,32 @@ import vhtml from "vhtml";
 import { BUILD_DIR } from "./config.js";
 
 export const remove = async (p) => await fs.rm(p, { force: true, recursive: true });
+
 export const makeDir = async (p) =>
 	await fs.mkdir(p, { recursive: true }).catch((e) => (e.code === "EEXIST" ? undefined : Promise.reject(e)));
+
 export const read = async (p, e = "utf8") => await fs.readFile(p, e);
+
 export const readDir = async (p) => await fs.readdir(p);
+
 export const write = async (p, d) => {
 	if (path.dirname(p) !== ".") {
 		await makeDir(path.join(BUILD_DIR, path.dirname(p)));
 	}
 	await fs.writeFile(path.join(BUILD_DIR, p), d);
 };
+
 export const copy = async (i, o = i) => await fs.cp(i, o, { recursive: true });
-export const renderMd = (d) => marked(d, { mangle: false, headerIds: false });
-export const renderHbs = (t, d = {}) => t({ ...DEFAULT_TEMPLATE_PROPERTIES, ...d });
+
 export const readMd = async (p) => {
 	const d = fm(await read(p));
-	const b = renderMd(d.body);
+	const b = marked(d.body, { mangle: false, headerIds: false });
 	return {
 		...d.attributes,
 		body: b,
 	};
 };
-export const readJson = async (p) => JSON.parse(await read(p));
+
 export function* paginate(xs, perPage, sort) {
 	xs = xs.slice();
 	xs.sort(sort);
@@ -45,9 +49,11 @@ export function* paginate(xs, perPage, sort) {
 		page++;
 	}
 }
-export const render = (o, C, ps = {}, ...children) =>
-	write(o, "<!DOCTYPE html>" + vhtml(C, { ...ps, showBlog: true }, ...children));
+
+export const render = async (o, C, as = {}, ...cs) => await write(o, "<!DOCTYPE html>" + vhtml(C, as, ...cs));
+
 export const mapP = (xs, fn) => Promise.all(xs.map(fn));
 
 export const join = (...ps) => path.join(...ps);
+
 export const basename = (p) => path.basename(p, path.extname(p));
