71038cdbf42ee3b234e69c8815fca522d7619445
Phoebe Clarke <me@phoebe.codes>
2023-05-28 12:50:11 -0400

    Add font preloads, null favicon

--- source/components/head.jsx
+++ source/components/head.jsx
@@ -8,7 +8,11 @@ export function Head({ title }) {
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta name="description" content="personal site for phoebe" />
 			<link rel="preload" href="/fonts/BerkeleyMono-Regular.woff2" as="font" type="font/woff2" crossorigin />
+			<link rel="preload" href="/fonts/BerkeleyMono-Italic.woff2" as="font" type="font/woff2" crossorigin />
+			<link rel="preload" href="/fonts/BerkeleyMono-Bold.woff2" as="font" type="font/woff2" crossorigin />
+			<link rel="preload" href="/fonts/BerkeleyMono-BoldItalic.woff2" as="font" type="font/woff2" crossorigin />
 			<link rel="stylesheet" href="/style.css" />
+			<link rel="icon" href="data:,"></link>
 		</head>
 	);
 }
