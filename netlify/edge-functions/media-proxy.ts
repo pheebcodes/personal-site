31226ca949d2c03b62bcf00481723542db4ea445
phoebe <me@phoebe.codes>
2025-01-20 23:19:05 -0500

    add caching headers to media proxy

---
+++ netlify/edge-functions/media-proxy.ts
@@ -0,0 +1,27 @@
+import { Context } from "https://edge.netlify.com";
+
+export default async function handler(
+	request: Request,
+	context: Context,
+): Promise<Response | undefined> {
+	const assetUrl = new URL(
+		"https://f005.backblazeb2.com/file/phoebecodes-personal-site-assets",
+	);
+	assetUrl.pathname += `/${context.params[0]}`;
+	const res = await fetch(assetUrl);
+	const lastModifiedNumber = Number(
+		res.headers.get("x-bz-info-src_last_modified_millis"),
+	);
+	const lastModified = new Date(
+		!isNaN(lastModifiedNumber) ? lastModifiedNumber : 0,
+	);
+	const etag = res.headers.get("x-bz-content-sha1");
+	return lastModified.getTime() !== 0 && etag
+		? new Response(res.body, {
+				headers: {
+					etag,
+					"last-modified": lastModified.toUTCString(),
+				},
+			})
+		: new Response(res.body);
+}
