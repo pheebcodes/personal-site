bcd8cb9ca09df6760db8d0b952c197595256ced5
phoebe <me@phoebe.codes>
2025-04-22 14:49:17 -0400

    add multiple mastodon aliases

--- netlify/edge-functions/webfinger-redirect.ts
+++ netlify/edge-functions/webfinger-redirect.ts
@@ -1,8 +1,15 @@
+const accounts = new Set([
+	"acct:me@phoebe.codes",
+	"acct:me@phoebeclarke.com",
+	"acct:phoebe@bitch.lgbt",
+	"acct:phoebe@anarchist.nyc",
+]);
+
 export default function handler(request: Request): Response | undefined {
 	try {
 		const url = new URL(request.url);
 		const resource = url.searchParams.get("resource");
-		if (resource !== "acct:me@phoebe.codes") {
+		if (resource && !accounts.has(resource)) {
 			return new Response("404 Not Found", {
 				status: 404,
 			});
