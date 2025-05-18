500d17c687ee405dd9b812563420ee6e473d06db
phoebe <me@phoebe.codes>
2025-05-17 22:36:11 -0400

    Use node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 export default defineConfig({
 	site: "https://phoebe.codes",
+	adapter: node({
+		mode: "standalone",
+	}),
 	redirects: {
 		"/blog": "/blog/toc",
 		"/blog/toc/1": "/blog/toc",
