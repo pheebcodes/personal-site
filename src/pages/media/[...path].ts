0a17778ae83e42cb4681b207e8b28f548e00f29f
phoebe <me@phoebe.codes>
2025-05-18 10:11:56 -0400

    Swap to scaleway object storage

--- src/pages/media/[...path].ts
+++ src/pages/media/[...path].ts
@@ -5,7 +5,7 @@ export const prerender = false;
 export async function GET({ params }: APIContext) {
 	const url = new URL(
 		params.path!,
-		"https://phoebecodes-personal-site-assets.s3.us-east-005.backblazeb2.com/"
+		"https://personal-site.s3.fr-par.scw.cloud"
 	);
 	return await fetch(url);
 }
