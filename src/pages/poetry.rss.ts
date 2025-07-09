5868cde57ffdfbba8fa05e3a9938d953f09092f6
phoebe <me@phoebe.codes>
2025-07-09 10:10:28 +0100

    2025-07-12 silence, support timed releases

--- src/pages/poetry.rss.ts
+++ src/pages/poetry.rss.ts
@@ -1,18 +1,22 @@
 import rss from "@astrojs/rss";
+import { UTCDate } from "@date-fns/utc";
 import type { APIContext } from "astro";
 import { getCollection } from "astro:content";
-import { compareDesc } from "date-fns";
+import { compareDesc, isAfter } from "date-fns";
 
-const posts = await getCollection("poems");
-const items = await Promise.all(
-	posts
+const collection = await getCollection(
+	"poems",
+	(poem) => import.meta.env.DEV || isAfter(poem.data.date, new UTCDate())
+);
+const poems = await Promise.all(
+	collection
 		.toSorted((a, b) => compareDesc(a.data.date, b.data.date))
-		.map(async (post) => ({
-			title: post.data.title,
-			link: `/poetry/poems/${post.id}`,
-			pubDate: post.data.date,
-			guid: post.id,
-			content: post.rendered!.html,
+		.map(async (poem) => ({
+			title: poem.data.title,
+			link: `/poetry/poems/${poem.id}`,
+			pubDate: poem.data.date,
+			guid: poem.id,
+			content: poem.rendered!.html,
 			author: "me@phoebe.codes (Phoebe Clarke)",
 		}))
 );
@@ -22,6 +26,6 @@ export function GET(context: APIContext) {
 		title: "phoebe.codes poetry",
 		description: "phoebe's poems",
 		site: context.site!,
-		items,
+		items: poems,
 	});
 }
